7799ea797d4ee240d9f48e6df73f35cf514c257a
stavramovic <stefan.avramovic096@gmail.com>
2021-09-03 23:21:23 +0200

    server.js update

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.urlencoded({ extended: false }))
 
 const port = 3000
 
-app.use('/css', express.static(__dirname + '/css'))
+app.use(express.static(__dirname))
+/* app.use('/css', express.static(__dirname + '/css'))
 app.use('/js', express.static(__dirname + '/js'))
-app.use('/img', express.static(__dirname + '/img'))
+app.use('/img', express.static(__dirname + '/img')) */
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
-});
+    res.sendFile(path.resolve(__dirname + '/index.html'))
+})
+
+/* app.all('*', function (req, res) {
+    res.status(404).send('resource not found')
+}) */
 
 app.post('/', function(req, res) {
 
-    console.log(req.body.first_name)
-    console.log(req.body.last_name)
-    console.log(req.body.email)
-    console.log(req.body.password)
-
-    res.end()
+    console.log(req.body)
 })
 
 app.listen(port, () => `Listening on port ${port}` )
