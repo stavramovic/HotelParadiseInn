671bca5cb1b98cd699a1ef8b1aa6d10009f06aab
stavramovic <stefan.avramovic096@gmail.com>
2021-09-20 22:53:52 +0200

    authentication custom callback

--- routes/users.js
+++ routes/users.js
@@ -7,25 +7,27 @@ const { ensureAuthenticated } = require('../config/auth')
 const { RegisterUser } = require('../controllers/users');
 
 
-router.get('/loggedInPage', ensureAuthenticated, (req, res) => {
-    res.render(path.resolve(__dirname, '..' + '/loggedInPage.ejs'))
+router.get('/login/user/:id', ensureAuthenticated, (req, res) => {
+    res.render('loggedInPage')
 })
 
 router.route('/register')
-.get((req,res) => res.render(path.resolve(__dirname, '..' + '/views/register.ejs')))
+.get((req,res) => res.render('register'))
 .post(RegisterUser)
 
 //Login Handle
 router.route('/login')
-.get((req,res) => res.render(path.resolve(__dirname, '..' + '/views/login.ejs')))
+.get((req,res) => res.render('login'))
 .post((req, res, next) => {
-    passport.authenticate('local', {
-        successRedirect: '/loggedInPage',
-        failureRedirect: '/login',
-        failureFlash: true
-    })  (req, res, next)
+    passport.authenticate('local', {failureFlash: true}, function(err, user, info) {
+        if(err) { return next(err) }
+        if(!user) { return res.redirect('/login') }
+        req.logIn(user, function(err) {
+            if(err) { return next(err) }
+        return res.redirect('/login/user/' + req.user.id)
+        })
+    })(req, res, next)
 })
-
 
 //Logout Handle
 router.get('/logout', (req,res) => {
