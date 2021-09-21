aadfa72a5132ea9febcb2a56e03f3562c4828089
stavramovic <stefan.avramovic096@gmail.com>
2021-09-22 00:02:02 +0200

    fixed errors

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,9 @@ const { ensureAuthenticated } = require('../config/auth')
 const { RegisterUser } = require('../controllers/users');
 
 
-router.get('/login/user/:id', ensureAuthenticated, (req, res) => {
+router.get('/login/user/', ensureAuthenticated, (req, res) => {
     res.render('loggedInPage')
+    /* name: req.user.name */
 })
 
 router.route('/register')
@@ -19,14 +20,11 @@ router.route('/register')
 router.route('/login')
 .get((req,res) => res.render('login'))
 .post((req, res, next) => {
-    passport.authenticate('local', {failureFlash: true}, function(err, user, info) {
-        if(err) { return next(err) }
-        if(!user) { return res.redirect('/login') }
-        req.logIn(user, function(err) {
-            if(err) { return next(err) }
-        return res.redirect('/login/user/' + req.user.id)
-        })
-    })(req, res, next)
+    passport.authenticate('local', {
+        successRedirect: '/login/user/',
+        failureRedirect: '/login',
+        failureFlash: true
+    })  (req, res, next)
 })
 
 //Logout Handle
